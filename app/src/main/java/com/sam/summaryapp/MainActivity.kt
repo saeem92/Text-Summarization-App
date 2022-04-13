bd9131b60276f3b6871318e053a82886b4de083f
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-04-13 05:41:56 +0530

    Built the camera module

--- app/src/main/java/com/sam/summaryapp/MainActivity.kt
+++ app/src/main/java/com/sam/summaryapp/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.sam.summaryapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var scanforimage: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        scanforimage = findViewById(R.id.ScanforImage)
+
+        scanforimage.setOnClickListener {
+
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            startActivity(intent)
+        }
     }
+}
+
+private fun Button.setOnClickListener(onClickListener: DialogInterface.OnClickListener) {
+    TODO("Not yet implemented")
 }
 
